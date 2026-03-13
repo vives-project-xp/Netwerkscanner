bf0db599b35c404f01b27216364db4942b248af4
Spotnit <spotnitcode@gmail.com>
2026-03-13 11:29:27 +0100

    blink led class

--- software/Netwerkscanner/main/Test.cpp
+++ software/Netwerkscanner/main/Test.cpp
@@ -1,13 +1,21 @@
 #include "Test.h"
 
+MyLed::MyLed()
+{
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+}
+
 void MyLed::on()
 {
     gpio_set_level(LED_GPIO, 1); // LED on
-    vTaskDelay(pdMS_TO_TICKS(100));
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    printf("on\n");
 }
 
 void MyLed::off()
 {
     gpio_set_level(LED_GPIO, 0); // LED off
-    vTaskDelay(pdMS_TO_TICKS(100));
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    printf("off\n");
 }
