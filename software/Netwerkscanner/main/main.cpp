2d3720ad7a80abccdc05d98358d6143dc73d2567
Spotnit <spotnitcode@gmail.com>
2026-03-25 19:35:12 +0100

    menu met knoppen werkt

--- software/Netwerkscanner/main/main.cpp
+++ software/Netwerkscanner/main/main.cpp
@@ -23,10 +23,25 @@
 
 static const char *LOGTAG = "MAIN.CPP";
 
-#define DEBOUNCE_DELAY_US 100000ULL // 100ms
-static volatile uint64_t last_isr_time = 0;
-static volatile uint32_t counter = 0;
-static QueueHandle_t button_queue;
+#define DEBOUNCE_DELAY 100000ULL      // 100ms
+#define GPIO_BUTTON_UP GPIO_NUM_5     // A4
+#define GPIO_BUTTON_DOWN GPIO_NUM_4   // A3
+#define GPIO_BUTTON_SELECT GPIO_NUM_2 // A1
+static volatile uint64_t lastIsrTimeUp = 0;
+static volatile uint64_t lastIsrTimeDown = 0;
+static volatile uint64_t lastIsrTimeSelect = 0;
+static volatile uint8_t buttonCountUp = 0;
+static volatile uint8_t buttonCountDown = 0;
+static volatile uint8_t buttonCountSelect = 0;
+
+typedef enum
+{
+    BUTTON_UP,
+    BUTTON_DOWN,
+    BUTTON_SELECT
+} ButtonEventT;
+
+static QueueHandle_t menuQueue;
 
 void ScanNetworks()
 {
@@ -62,30 +77,93 @@ void CheckCpuFreq()
 
 void MenuTask(void *pvParameters)
 {
+    ButtonEventT ontvangen_event;
+    int8_t menu_index = 0;
+
     while (1)
     {
-        ESP_LOGI(LOGTAG, "Hallo van task menu");
+        // Wacht in ruststand tot er IETS in de brievenbus komt
+        if (xQueueReceive(menuQueue, &ontvangen_event, portMAX_DELAY))
+        {
 
-        // Wacht 1000ms (vTaskDelay verwacht ticks, pdMS_TO_TICKS rekent dit om)
-        vTaskDelay(pdMS_TO_TICKS(1000));
+            if (ontvangen_event == BUTTON_UP)
+            {
+                menu_index++;
+                printf("Cursor omhoog naar: %d\n", menu_index);
+            }
+            else if (ontvangen_event == BUTTON_DOWN)
+            {
+                menu_index--;
+                printf("Cursor omlaag naar: %d\n", menu_index);
+            }
+            else if (ontvangen_event == BUTTON_SELECT)
+            {
+                printf("GEKOZEN: Je hebt item %d geselecteerd!\n", menu_index);
+                // Voer hier de actie uit die bij dit menu-item hoort
+            }
+        }
     }
 }
 
-static void IRAM_ATTR button_isr(void *arg)
+static void IRAM_ATTR buttonIsrUp(void *arg)
 {
     uint64_t now = esp_timer_get_time();
-    if (now - last_isr_time > DEBOUNCE_DELAY_US)
+    if (now - lastIsrTimeUp > DEBOUNCE_DELAY)
     {
-        counter = counter + 1;
-        uint32_t cnt = counter;
-        BaseType_t higher_priority_task_woken = pdFALSE;
-        xQueueSendFromISR(button_queue, &cnt, &higher_priority_task_woken); // Send counter to queue from ISR
-        last_isr_time = now;
-        if (higher_priority_task_woken)
-        {
-            portYIELD_FROM_ISR();
-        }
+        ButtonEventT event = BUTTON_UP;
+        xQueueSendFromISR(menuQueue, &event, NULL);
+        lastIsrTimeUp = now;
     }
+}
+
+static void IRAM_ATTR buttonIsrDown(void *arg)
+{
+    uint64_t now = esp_timer_get_time();
+    if (now - lastIsrTimeDown > DEBOUNCE_DELAY)
+    {
+        ButtonEventT event = BUTTON_DOWN;
+        xQueueSendFromISR(menuQueue, &event, NULL);
+        lastIsrTimeDown = now;
+    }
+}
+
+static void IRAM_ATTR buttonIsrSelect(void *arg)
+{
+    uint64_t now = esp_timer_get_time();
+    if (now - lastIsrTimeSelect > DEBOUNCE_DELAY)
+    {
+        ButtonEventT event = BUTTON_SELECT;
+        xQueueSendFromISR(menuQueue, &event, NULL);
+        lastIsrTimeSelect = now;
+    }
+}
+
+void CreatButtonInterrupts()
+{
+
+    menuQueue = xQueueCreate(10, sizeof(uint8_t));
+
+    gpio_config_t io_conf = {};
+    io_conf.intr_type = GPIO_INTR_NEGEDGE; // falling edge
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pin_bit_mask = (1ULL << GPIO_BUTTON_UP) |
+                           (1ULL << GPIO_BUTTON_DOWN) |
+                           (1ULL << GPIO_BUTTON_SELECT);
+    io_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    gpio_config(&io_conf);
+
+    gpio_install_isr_service(0);
+
+    gpio_isr_handler_add(GPIO_BUTTON_UP, buttonIsrUp, NULL);
+    gpio_isr_handler_add(GPIO_BUTTON_DOWN, buttonIsrDown, NULL);
+    gpio_isr_handler_add(GPIO_BUTTON_SELECT, buttonIsrSelect, NULL);
+}
+
+void GpioSetup()
+{
+    GpioScreenSetup();
+    CreatButtonInterrupts();
 }
 
 extern "C" void app_main(void)
@@ -95,40 +173,21 @@ extern "C" void app_main(void)
     printf("Build Date: %s\n", __DATE__);
     printf("Build Time: %s\n", __TIME__);
 
-    button_queue = xQueueCreate(10, sizeof(uint32_t));
-
-    gpio_config_t io_conf = {};
-    io_conf.intr_type = GPIO_INTR_POSEDGE;
-    io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pin_bit_mask = (1ULL << GPIO_NUM_4);
-    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
-    gpio_config(&io_conf);
-
-    gpio_install_isr_service(0);
-    gpio_isr_handler_add(GPIO_NUM_4, button_isr, NULL);
-
-    uint32_t button_counter;
-
-    // Keep program running
-    while (1)
-    {
-        // Wait indefinitely for an item in the queue
-        if (xQueueReceive(button_queue, &button_counter, portMAX_DELAY))
-        {
-            printf("Button pressed %lu times.\n", button_counter);
-        }
-    }
+    GpioSetup();
 
     xTaskCreate(
         MenuTask,   // Naam van de functie
         "MenuTask", // Naam voor debugging
         4096,       // Stack size in bytes
         NULL,       // Parameter die je mee wilt geven (optioneel)
-        1,          // Prioriteit (hoger getal = hogere prioriteit)
+        5,          // Prioriteit (hoger getal = hogere prioriteit)
         NULL        // Task handle (optioneel, om de task later aan te sturen)
     );
 
+    while (1)
+    {
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
     //
     // ScreenTest();
 
