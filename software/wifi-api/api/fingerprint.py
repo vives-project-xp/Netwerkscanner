a84e0a945b6dfe04e581d4897c84ea9191a54f18
root <root@NetworkScanner.lan>
2026-04-24 16:54:52 +0000

    findLocation elke locatie weet hoe goed het overeen komt

--- software/wifi-api/api/fingerprint.py
+++ software/wifi-api/api/fingerprint.py
@@ -3,6 +3,11 @@ import math
 import os
 
 import mysql.connector
+#terminal van container
+#docker exec -it wifi_api /bin/bash 
+#hermaken van de container als je een aanpassing hebt gemaakt
+#docker compose up -d --build wifi_api 
+#docker logs wifi_api
 
 # Functie om verbinding te maken met de database
 def get_db():
@@ -27,6 +32,92 @@ def get_db():
 #is het verschil groter dan x -> markeren als komt niet overeen
 #(punt komt met niets overeen in de fingerprinting database)
 
+#Tables in DB: [('bluetooth',), ('dev',), ('fingerprint',), ('heatmap',)]
+#id, scan_id, device_id, scan_time_start, scan_time_end, x, y, manual, net_ssid, net_bssid, primary_channel, secondary_channel, net_rssi, auth_mode, pairwise_cipher, group_cipher, antenna, phy_modes_11b, phy_modes_11g, phy_modes_11n, phy_modes_11a, phy_modes_11ac, phy_modes_11ax, phy_modes_lr, wps, ftm_responder, ftm_initiator, bandwidth, vht_freq1, vht_freq2, he_ap_bss_color, he_ap_partial_bss_color, he_ap_bss_color_disabled, he_ap_bssid_index, reserved, country
+#http://10.20.10.24:8081/fingerprint
+
+
+
+def findLocation(scan_id):
+    db = get_db()
+    cursor = db.cursor()
+
+    cursor.execute("SELECT net_bssid, net_rssi FROM heatmap WHERE scan_id = %s", (scan_id,))
+    scanDict = dict(cursor.fetchall())
+
+    cursor.execute("SELECT DISTINCT scan_id FROM fingerprint WHERE manual = 1")
+    bestaandeScan_ids = [row[0] for row in cursor.fetchall()]
+
+    fingerprintScores = []
+
+    #overloop alle posities in fingerprint table
+    for i in bestaandeScan_ids:
+        print("loop fingerprint table. scan_id="+str(i))
+
+        differenceSum = 0
+        matches = 0
+        #overloop alle netwerken op deze locatie
+        query = """
+            SELECT net_bssid, net_rssi 
+            FROM fingerprint 
+            WHERE scan_id = %s AND manual = 1
+        """
+        
+        cursor.execute(query, (i,))
+        scanFingerprintDict = dict(cursor.fetchall())
+        for bssidFP, rssiFP in scanFingerprintDict.items():
+            if bssidFP in scanDict:
+                rssiScan = scanDict[bssidFP]
+                differenceSum += abs(rssiFP - rssiScan)
+                matches +=1
+        if matches > 0:
+        # We voegen een dictionary toe aan de array
+            fingerprintScores.append({
+                'scan_id': i,
+                'differenceAverage': differenceSum/matches
+            })
+
+
+    print(fingerprintScores)
+
+
+
+
+    db.close()
+
+findLocation(4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!!!!!!!!!!!!!!app.py heeft dit voorlopig nog nodig!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # Bereken de afstand tussen gescande netwerken en handmatige netwerken op basis van RSSI-waarden
 def rssi_distance(scan_networks, manual_networks):
     dist = 0
@@ -43,9 +134,9 @@ def predict_location(scan_id):
     cursor = db.cursor(dictionary=True)
 
     # Haal netwerken van de nieuwe scan op
-    cursor.execute("SELECT bssid, rssi FROM networks WHERE scan_id = %s", (scan_id,))
+    cursor.execute("SELECT net_bssid, rssi FROM networks WHERE scan_id = %s", (scan_id,))
     scan_nets = cursor.fetchall()
-    scan_map = {row["bssid"]: row["rssi"] for row in scan_nets}
+    scan_map = {row["net_bssid"]: row["rssi"] for row in scan_nets}
 
     # Haal alle handmatige punten op
     cursor.execute("SELECT id, x, y FROM scans WHERE manual = 1")
@@ -55,9 +146,9 @@ def predict_location(scan_id):
     best_distance = float("inf")
 
     for point in manual_points:
-        cursor.execute("SELECT bssid, rssi FROM networks WHERE scan_id = %s", (point["id"],))
+        cursor.execute("SELECT net_bssid, rssi FROM networks WHERE scan_id = %s", (point["id"],))
         nets = cursor.fetchall()
-        manual_map = {row["bssid"]: row["rssi"] for row in nets}
+        manual_map = {row["net_bssid"]: row["rssi"] for row in nets}
 
         d = rssi_distance(scan_map, manual_map)
 
@@ -85,3 +176,4 @@ def update_scan_with_prediction(scan_id):
 
     db.commit()
     return True
+print("hello van fingerprint.py")
